81cf8452dd23c90a5dc9fc52b6781826f848b3b5
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-31 00:21:22 +0300

    Installation of the notification app, setting up of the models serializers, urls, viewset and migrations

---
+++ notifications/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+User = get_user_model()
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, related_name="notifications", on_delete=models.CASCADE)
+    actor = models.ForeignKey(User, related_name="actor_notifications", on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)  # For "liked your post", "commented on your post", "followed you"
+    # Generic relation (post, comment, follow)
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    target_object_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_content_type", "target_object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb}"
